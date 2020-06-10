9e933946a094d5b73e435076bffb97924eeb1d17
Ryan Govostes <rgovostes@whoi.edu>
2020-06-10 09:30:01 -0400

    Use the ds_ros NMEA node to parse messages

--- packages/pa_base/src/web_node.py
+++ packages/pa_base/src/web_node.py
@@ -2,29 +2,35 @@
 import flask
 import rospy
 
-from ds_core_msgs.msg import RawData
+from ds_nmea_msgs.msg import Gga
 
 
-last_message = ''
+last_location = {}
 
 def on_message(msg):
-    global last_message
-    last_message = msg.data
+    global last_location
+    sign = { 'N': 1, 'E': 1, 'S': -1, 'W': -1 }
+    last_location = {
+        'latitude':  msg.latitude * sign[chr(msg.latitude_dir)],
+        'longitude': msg.longitude * sign[chr(msg.longitude_dir)],
+    }
 
 
 app = flask.Flask(__name__)
 
 @app.route('/')
 def index():
-    return last_message
+    return flask.jsonify({
+        'location': last_location,
+    })
 
 
 def main():
     # Note that rospy spawns threads for message dispatch, so we don't have to
     rospy.init_node('web_node')
-    rospy.Subscriber('/nmea_listener/in', RawData, on_message)
+    rospy.Subscriber('/nmea_listener/nmea_gga', Gga, on_message)
 
-    app.run(host='0.0.0.0', port=8000)
+    app.run(host='0.0.0.0', port=8092)
 
 
 if __name__ == '__main__':
