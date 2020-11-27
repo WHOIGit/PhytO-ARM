56b72db13f93cbe3968bbf5db8cec8ebc14ddb3c
Ryan Govostes <rgovostes@whoi.edu>
2020-11-27 13:10:31 -0500

    Publish a custom raw register value message

--- src/pa_base/src/jvl_motor_node.py
+++ src/pa_base/src/jvl_motor_node.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 
 import rospy
-import std_msgs.msg
 
 import mac400registers
 
 from pyModbusTCP.client import ModbusClient
+
+from pa_base.msg import JVLMotorRegisterRaw
 
 
 def read_registers(client, registers):
@@ -13,7 +14,7 @@ def read_registers(client, registers):
     Efficiently requests values for a set of registers, attempting to minimize
     the number of requests and the time difference between requests.
 
-    Returns a dictionary of Register -> (decoded value, timestamp)
+    Returns a dictionary of Register -> (raw values, timestamp)
     '''
     # Group registers into contiguous ranges that can be read at one time
     registers = sorted(registers, key=lambda r: r.addr[0])
@@ -34,26 +35,33 @@ def read_registers(client, registers):
     output = {}
     for grp, values, timestamp in zip(groups, valueses, timestamps):
         for reg in grp:
-            decoded = reg.decode(*[ values[a - grp[0].addr[0]]
-                                    for a in reg.addr ])
-            output[reg] = (decoded, timestamp)
+            these_values = [ values[a - grp[0].addr[0]] for a in reg.addr ]
+            output[reg] = (these_values, timestamp)
     return output
 
 
 def main():
+    # Initialize this node
+    rospy.init_node('jvl_motor', anonymous=True)
+
     # Look up the requested registers
-    registers = [ mac400registers.register_for_name(reg) \
-                  for reg in rospy.get_param('/jvl_motor/registers') ]
+    registers = [
+        mac400registers.P_IST,
+        mac400registers.V_IST_16,
+        mac400registers.FLWERR,
+        mac400registers.U_24V,
+        mac400registers.ERR_STAT,
+        mac400registers.U_BUS,
+    ]
 
     # Create a publisher for each register we are monitoring
     publishers = {}
     for reg in registers:
-        publishers[reg] = rospy.Publisher(f'/jvl/register/{reg.name}',
-                                          std_msgs.msg.Int32,
-                                          queue_size=10)
-
-    # Initialize this node
-    rospy.init_node('jvl_motor', anonymous=True)
+        publishers[reg] = rospy.Publisher(
+            f'{rospy.get_name()}/register/{reg.name}',
+            JVLMotorRegisterRaw,
+            queue_size=1
+        )
 
     # Connect to the Modbus server
     client = ModbusClient(
@@ -65,8 +73,12 @@ def main():
     rate = rospy.Rate(rospy.get_param('/jvl_motor/refresh_rate', 1))  # hz
     while not rospy.is_shutdown():
         reg_values = read_registers(client, registers)
-        for reg, (value, timestamp) in reg_values.items():
-            publishers[reg].publish(value)
+        for reg, (values, timestamp) in reg_values.items():
+            msg = JVLMotorRegisterRaw()
+            msg.header.stamp = msg.ds_header.io_time = timestamp
+            msg.raw_values = values
+
+            publishers[reg].publish(msg)
         rate.sleep()
 
 
