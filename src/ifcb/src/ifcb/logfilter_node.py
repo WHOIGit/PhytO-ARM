3c4c48ef41c7495d627b84a0b6d817374cb42e26
Ryan Govostes <rgovostes@whoi.edu>
2022-08-05 01:02:43 -0400

    Add node for filtering noisy IFCB raw messages out of rosbags

---
+++ src/ifcb/src/ifcb/logfilter_node.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+import functools
+
+import rospy
+
+from ds_core_msgs.msg import RawData
+
+
+# Republish incoming "raw" messages, dropping data blobs
+def on_message(pub, msg):
+    # Omit the binary blobs while keeping all the other fields
+    if msg.data.startswith(b'triggerimage:'):
+        msg.data = b'triggerimage:<omitted>'
+    elif msg.data.startswith(b'triggerrois:'):
+        parts = msg.data.split(b':')
+        for i in range(4, len(parts), 3):
+            parts[i] = b'<omitted>'
+        msg.data = b':'.join(parts)
+    elif msg.data.startswith(b'file:chunk:'):
+        parts = msg.data.split(b':', maxsplit=4)
+        parts[-1] = b'<omitted>'
+        msg.data = b':'.join(parts)
+
+    # Republish the message, which has potentially been mutated
+    pub.publish(msg)
+
+
+def main():
+    rospy.init_node('ifcb_logfilter', anonymous=True)
+
+    # Publishers for raw messages coming in and out of the IFCB
+    pub = rospy.Publisher('/ifcb/in/filtered', RawData, queue_size=5)
+    rospy.Subscriber('/ifcb/in', RawData, functools.partial(on_message, pub))
+
+    # Keep the program alive while other threads work
+    rospy.spin()
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except rospy.ROSInterruptException:
+        pass
