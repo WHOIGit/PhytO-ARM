da53256ffa3300bbd8fc96c454b581849c672a2c
John Walsh <johnwalsh7412@gmail.com>
2024-10-08 17:59:12 -0400

    correct logger request format and add example config

--- src/phyto_arm/src/digital_logger_node.py
+++ src/phyto_arm/src/digital_logger_node.py
@@ -7,6 +7,9 @@ from requests.auth import HTTPDigestAuth
 from phyto_arm.msg import OutletStatus
 
 def control_outlet(msg):
+    """
+    Send the given msg to the digital logger as an HTTP request.
+    """
     username = rospy.get_param('/digital_logger/username')
     password = rospy.get_param('/digital_logger/password')
     address = rospy.get_param('/digital_logger/address')
@@ -14,17 +17,19 @@ def control_outlet(msg):
 
     for outlet in outlets:
         if outlet['name'] == msg.name:
+            # outlet numbers in the config yaml start from 1 whereas outlet numbers in the API start from 0
             outlet_num = int(outlet['outlet']) - 1
 
     status = msg.status == 'on'
 
-    response = requests.put(f'http://{address}/restapi/relay/outlets/{outlet_num}/state/', auth=HTTPDigestAuth(username, password), data={'value': status}, headers={"X-CSRF": "x", "Accept": "application/json"})
-    rospy.logwarn(f'sent: http://{address}/restapi/relay/outlets/{outlet_num}/state/, {status}, {response.text}')
+    response = requests.put(f'http://{address}/restapi/relay/outlets/{outlet_num}/state/', auth=HTTPDigestAuth(username, password), data={'value': str(status).lower()}, headers={"X-CSRF": "x", "Accept": "application/json"})
+    rospy.loginfo(f'sent: http://{address}/restapi/relay/outlets/{outlet_num}/state/, auth={username},{password} status={str(status).lower()}, received: code {response.status_code} : {response.text}')
 
 
 def main():
     rospy.init_node('digital_logger_node')
 
+    # subscribe to the digital logger control topic
     subscriber = rospy.Subscriber('/digital_logger/control', OutletStatus, control_outlet)
 
     username = rospy.get_param('/digital_logger/username')
@@ -33,10 +38,12 @@ def main():
     outlets = rospy.get_param('/digital_logger/outlets')
     num_outlets = len(outlets)
 
+    # create an independent publisher for each outlet
     outlet_publishers = {}
     for outlet_num in range(num_outlets):
         outlet_publishers[f'outlet_{outlet_num}'] = rospy.Publisher(f'/digital_logger/outlet/{outlet_num}/status/', OutletStatus, queue_size=10)
 
+    # Monitor outlets at 1Hz
     rate = rospy.Rate(1)
 
     while not rospy.is_shutdown():
@@ -46,6 +53,7 @@ def main():
 
         assert len(result) == num_outlets
 
+        # publish the status of each outlet to its specific topic
         for outlet_index in range(len(result)):
             if result[outlet_index]:
                 status = 'on'
