e0d279be0d410599a8b3e7311978ee14f178a727
Nathan Figueroa <nathan.figueroa@whoi.edu>
2024-12-23 17:15:54 -0500

    refactor: Moved udp_to_ros to use preferred classless node paradigm

--- src/phyto_arm/src/udp_to_ros.py
+++ src/phyto_arm/src/udp_to_ros.py
@@ -6,109 +6,107 @@ import json
 from ds_core_msgs.msg import RawData
 from std_msgs.msg import Float32, String, Int32, Float32MultiArray, Int32MultiArray
 
-class UdpToRosNode:
-    def __init__(self):
-        rospy.init_node('udp_to_ros_node', anonymous=True)
+publishers = {}
+raw_pub = None
+sock = None
+buffer = b""
+decoder = json.JSONDecoder()
 
-        self.parse_json = rospy.get_param('~parse_json', False)
-        self.base_topic = rospy.get_param('~topic', 'udp_stream')
-        self.UDP_IP = "0.0.0.0"
-        self.UDP_PORT = rospy.get_param('~port')
-
-        rospy.loginfo(f"Listening for UDP data on {self.UDP_IP}:{self.UDP_PORT}...")
-        rospy.loginfo(f"Base topic: {self.base_topic}")
-        rospy.loginfo(f"JSON parsing: {'enabled' if self.parse_json else 'disabled'}")
-
-        self.publishers = {}
-        self.raw_pub = None
-        if not self.parse_json:
-            self.raw_pub = rospy.Publisher(self.base_topic, RawData, queue_size=10)
-
-        try:
-            self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-            self.sock.bind((self.UDP_IP, self.UDP_PORT))
-        except socket.error as e:
-            rospy.logerr(f"Failed to create UDP socket: {str(e)}")
-            raise
-
-        self.buffer = b""
-        self.decoder = json.JSONDecoder()
-
-    def get_ros_msg_type(self, value):
-        if isinstance(value, float):
-            return Float32
-        elif isinstance(value, int):
-            return Int32
-        elif isinstance(value, str):
-            return String
-        elif isinstance(value, list):
-            if all(isinstance(x, float) for x in value):
-                return Float32MultiArray
-            elif all(isinstance(x, int) for x in value):
-                return Int32MultiArray
+def get_ros_msg_type(value):
+    if isinstance(value, float):
+        return Float32
+    elif isinstance(value, int):
+        return Int32
+    elif isinstance(value, str):
         return String
+    elif isinstance(value, list):
+        if all(isinstance(x, float) for x in value):
+            return Float32MultiArray
+        elif all(isinstance(x, int) for x in value):
+            return Int32MultiArray
+    return String
 
-    def publish_message(self, json_data):
-        for key, value in json_data.items():
-            if key not in self.publishers:
-                msg_type = self.get_ros_msg_type(value)
-                self.publishers[key] = {
-                    'publisher': rospy.Publisher(f"{self.base_topic}/{key}", msg_type, queue_size=10),
-                    'msg_type': msg_type
-                }
+def publish_message(json_data, base_topic):
+    for key, value in json_data.items():
+        if key not in publishers:
+            msg_type = get_ros_msg_type(value)
+            publishers[key] = {
+                'publisher': rospy.Publisher(f"{base_topic}/{key}", msg_type, queue_size=10),
+                'msg_type': msg_type
+            }
 
-            if isinstance(value, list):
-                if all(isinstance(x, float) for x in value):
-                    msg = Float32MultiArray(data=value)
-                elif all(isinstance(x, int) for x in value):
-                    msg = Int32MultiArray(data=value)
-                else:
-                    msg = String(data=str(value))
+        if isinstance(value, list):
+            if all(isinstance(x, float) for x in value):
+                msg = Float32MultiArray(data=value)
+            elif all(isinstance(x, int) for x in value):
+                msg = Int32MultiArray(data=value)
             else:
-                msg = self.publishers[key]['msg_type'](data=value)
+                msg = String(data=str(value))
+        else:
+            msg = publishers[key]['msg_type'](data=value)
 
-            self.publishers[key]['publisher'].publish(msg)
+        publishers[key]['publisher'].publish(msg)
 
-    def publish_raw_message(self, data):
-        msg = RawData()
-        msg.data = list(data)
-        msg.data_direction = RawData.DATA_IN
-        current_time = rospy.Time.now()
-        msg.ds_header.io_time = current_time
-        msg.header.stamp = current_time
-        self.raw_pub.publish(msg)
+def publish_raw_message(data):
+    msg = RawData()
+    msg.data = list(data)
+    msg.data_direction = RawData.DATA_IN
+    current_time = rospy.Time.now()
+    msg.ds_header.io_time = current_time
+    msg.header.stamp = current_time
+    raw_pub.publish(msg)
 
-    def run(self):
-        while not rospy.is_shutdown():
-            try:
-                data, _ = self.sock.recvfrom(65536)  # Increased buffer size
-                self.buffer += data
+def main():
+    global raw_pub, sock, buffer
+    rospy.init_node('udp_to_ros_node', anonymous=True)
 
-                if self.parse_json:
-                    while self.buffer:
-                        try:
-                            result, index = self.decoder.raw_decode(self.buffer.decode('utf-8'))
-                            message = self.buffer[:index].strip()
-                            self.buffer = self.buffer[index:]
+    parse_json = rospy.get_param('~parse_json', False)
+    base_topic = rospy.get_param('~topic', 'udp_stream')
+    UDP_IP = "0.0.0.0"
+    UDP_PORT = rospy.get_param('~port')
 
-                            json_data = json.loads(message)
-                            self.publish_message(json_data)
-                        except json.JSONDecodeError:
-                            # If we can't decode a complete object, wait for more data
-                            break
-                else:
-                    while b'\n' in self.buffer:
-                        newline_index = self.buffer.find(b'\n')
-                        message = self.buffer[:newline_index]
-                        self.publish_raw_message(message)
-                        self.buffer = self.buffer[newline_index+1:]
+    rospy.loginfo(f"Listening for UDP data on {UDP_IP}:{UDP_PORT}...")
+    rospy.loginfo(f"Base topic: {base_topic}")
+    rospy.loginfo(f"JSON parsing: {'enabled' if parse_json else 'disabled'}")
 
-            except socket.error as e:
-                rospy.logerr(f"Error receiving UDP data: {str(e)}")
+    if not parse_json:
+        raw_pub = rospy.Publisher(base_topic, RawData, queue_size=10)
+
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.bind((UDP_IP, UDP_PORT))
+    except socket.error as e:
+        rospy.logerr(f"Failed to create UDP socket: {str(e)}")
+        raise
+
+    while not rospy.is_shutdown():
+        try:
+            data, _ = sock.recvfrom(65536)
+            buffer += data
+
+            if parse_json:
+                while buffer:
+                    try:
+                        _, index = decoder.raw_decode(buffer.decode('utf-8'))
+                        message = buffer[:index].strip()
+                        buffer = buffer[index:]
+
+                        json_data = json.loads(message)
+                        publish_message(json_data, base_topic)
+                    except json.JSONDecodeError:
+                        break
+            else:
+                while b'\n' in buffer:
+                    newline_index = buffer.find(b'\n')
+                    message = buffer[:newline_index]
+                    publish_raw_message(message)
+                    buffer = buffer[newline_index+1:]
+
+        except socket.error as e:
+            rospy.logerr(f"Error receiving UDP data: {str(e)}")
 
 if __name__ == '__main__':
     try:
-        node = UdpToRosNode()
-        node.run()
+        main()
     except rospy.ROSInterruptException:
         pass
