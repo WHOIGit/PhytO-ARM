2433e27109bc2a183dc3b10af4f96fd3fa0377f3
Ryan Govostes <rgovostes@whoi.edu>
2025-06-17 13:34:03 -0700

    Refactor web requests and use digest auth

--- src/dli_power_switch/src/dli_power_switch_node.py
+++ src/dli_power_switch/src/dli_power_switch_node.py
@@ -2,7 +2,6 @@
 """
 Functionality for monitoring and controlling a Digital Loggers Web Power Switch Pro model.
 """
-import base64
 import urllib.request
 
 import rospy
@@ -11,62 +10,70 @@ from std_msgs.msg import Bool
 
 from dli_power_switch.msg import OutletStatus
 
-
-# Global variables relevant to digital logger control. These variables are defined once the digital
-# logger node is initialized.
-AUTH = ''
-ADDRESS = ''
-OUTLETS = ''
+from typing import Any, Dict, List, Optional
 
 
-def control_outlet(msg, number):
+# Global variables set at node initialization
+ADDRESS: str = ''
+OUTLETS: List[Dict[str, Any]] = []
+OPENER: Optional[urllib.request.OpenerDirector] = None
+
+
+# Configure a urllib opener for HTTP Digest authentication
+def configure_digest_opener(username: str, password: str) -> None:
+    global OPENER
+    pwd_mgr = urllib.request.HTTPPasswordMgrWithDefaultRealm()
+    pwd_mgr.add_password(None, f'http://{ADDRESS}/', username, password)
+    handler = urllib.request.HTTPDigestAuthHandler(pwd_mgr)
+    OPENER = urllib.request.build_opener(handler)
+
+
+def set_outlet_state(number: int, is_active: bool) -> None:
+    OPENER.open(urllib.request.Request(
+        f'http://{ADDRESS}/restapi/relay/outlets/{number}/state/',
+        data=f'value={str(is_active).lower()}'.encode(),
+        method='PUT',
+        headers={
+            'X-CSRF': 'x',
+        }
+    ))
+
+
+def get_outlet_state(number: int) -> bool:
+    response = OPENER.open(urllib.request.Request(
+        f'http://{ADDRESS}/restapi/relay/outlets/{number}/state/'
+    ))
+    return response.read() == b'true'
+
+
+def control_outlet(msg: Bool, number: int) -> None:
     """
     Send the given msg to the digital logger as an HTTP request.
     """
-    # Create a PUT request
-    url = f'http://{ADDRESS}/restapi/relay/outlets/{number}/state/'
-    req = urllib.request.Request(
-        url,
-        data=f'value={str(msg.data).lower()}'.encode(),
-        method='PUT',
-        headers={
-            'Authorization': AUTH,
-            'X-CSRF': 'x',
-        }
-    )
-
-    try:
-        # Send the PUT request
-        response = urllib.request.urlopen(req)
-    except urllib.error.HTTPError as http_err:
-        raise ValueError(f"HTTP Error: {http_err.code} : {http_err.reason}") from http_err
-    except urllib.error.URLError as url_err:
-        raise ValueError(f"URL Error: {url_err.reason}") from url_err
-
-    result = response.read().decode('utf-8')
-
-    rospy.loginfo(f'sent status={str(msg.data).lower()} to {ADDRESS}:{url}, '
-                  f'received: code {response.status} : {result}')
+    set_outlet_state(number, msg.data)
+    rospy.loginfo(f'Switched {"ON" if msg.data else "OFF"} outlet {number}')
 
 
-def run_dli_power_switch_node():
+def run_dli_power_switch_node() -> None:
     """
     Run the Digital Logger power switch node. Publishes outlet statuses at
     ~outlet/{name}/status. The outlets can be controlled by publishing a
     std_msgs/Bool message to ~outlet/{name}/control.
     """
-    global AUTH, ADDRESS, OUTLETS
+    global ADDRESS, OUTLETS
 
     rospy.init_node('dli_power_switch')
 
-    username = rospy.get_param('~username')
-    password = rospy.get_param('~password')
-    AUTH = f"Basic {base64.b64encode(f'{username}:{password}'.encode()).decode()}"
     ADDRESS = rospy.get_param('~address')
     OUTLETS = rospy.get_param('~outlets')
 
+    configure_digest_opener(
+        rospy.get_param('~username'),
+        rospy.get_param('~password')
+    )
+
     # Set up publisher and subscriber for each outlet
-    outlet_pubs = {}
+    outlet_pubs: Dict[int, rospy.Publisher] = {}
     for outlet in OUTLETS:
         name, num = outlet['name'], outlet['outlet']
         outlet_pubs[num] = rospy.Publisher(
@@ -83,23 +90,10 @@ def run_dli_power_switch_node():
         # send a status request for each configured outlet
         for outlet in OUTLETS:
             num = outlet['outlet']
-            url = f'http://{ADDRESS}/restapi/relay/outlets/{num}/state/'
-            request = urllib.request.Request(url)
-            request.add_header("Authorization", AUTH)
-
-            try:
-                with urllib.request.urlopen(request) as response:
-                    response_data = response.read().decode('utf-8')
-            except urllib.error.HTTPError as http_err:
-                raise ValueError(f"HTTP Error: {http_err.code} : {http_err.reason}") from http_err
-            except urllib.error.URLError as url_err:
-                raise ValueError(f"URL Error: {url_err.reason}") from url_err
-
-            # publish the status with timestamps and active flag
             outlet_status = OutletStatus()
+            outlet_status.is_active = get_outlet_state(num)
             outlet_status.header.stamp = outlet_status.ds_header.io_time = \
                 rospy.Time.now()
-            outlet_status.is_active = response_data == 'true'
             outlet_pubs[num].publish(outlet_status)
 
         rate.sleep()
