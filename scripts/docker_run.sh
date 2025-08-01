d0e5db38ba1eb464a7e0807c7ab0afa1ffa20888
Nathan Figueroa <nathan.figueroa@whoi.edu>
2025-08-01 15:18:27 -0400

    Not using CTD

--- scripts/docker_run.sh
+++ scripts/docker_run.sh
@@ -36,12 +36,10 @@ docker run --rm -it \
     --name phyto-arm \
     --publish 9090:9090/tcp \
     --publish 8098:8098/tcp \
-    --publish 12345:12345/udp \
     --mount type=bind,source="$(pwd)"/configs,target=/app/configs,readonly \
     --mount type=bind,source=$CONFIG,target=/app/mounted_config.yaml,readonly \
     --mount type=bind,source=/home/embedpi/routines,target=/routines,readonly \
     --mount type=bind,source="$(pwd)"/src/phyto_arm,target=/app/src/phyto_arm,readonly \
     --volume /data:/data \
-    --device /dev/ttyS3 \
     whoi/phyto-arm:nathan.figueroa-gps-lockout \
     $COMMAND
