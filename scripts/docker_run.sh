4a2f4c7b86a71c0279bbace2695630d7591c449c
Nathan Figueroa <nathan.figueroa@whoi.edu>
2026-02-20 15:33:36 -0500

    Revert some changes to enable docker_run to run as systemd without launchpad

--- scripts/docker_run.sh
+++ scripts/docker_run.sh
@@ -1,24 +1,21 @@
 #!/bin/bash
+
 # Default config path
 CONFIG='./configs/config.yaml'
+
 # Default command
-COMMAND=''
+COMMAND='./phyto-arm start main mounted_config.yaml'
 
 # Parse command-line options
-while getopts ":bdh" opt; do
+while getopts ":bh" opt; do
     case ${opt} in
         b)
             COMMAND='bash'
             ;;
-        d)
-            # Use container default (dashboard)
-            COMMAND=''
-            ;;
         h)
-            echo "Usage: ./scripts/docker_run.sh [-b] [-d] <config file path>"
+            echo "Usage: ./scripts/docker_run.sh [-b] <config file path>"
             echo "-h: Help message."
             echo "-b: Open shell into container instead of launching phyto-arm."
-            echo "-d: Run in daemon mode (uses container's default command)."
             echo "<config file path>: Defaults to ./configs/config.yaml if none provided."
             exit 1
             ;;
@@ -45,7 +42,6 @@ DOCKER_FLAGS+=("--rm")
 
 docker run "${DOCKER_FLAGS[@]}" \
     --name phyto-arm \
-    -e NO_VIRTUALENV=1 \
     --publish 8080:8080/tcp \
     --publish 9090:9090/tcp \
     --publish 8098:8098/tcp \
@@ -57,5 +53,4 @@ docker run "${DOCKER_FLAGS[@]}" \
     --mount type=bind,source="$CONFIG",target=/app/mounted_config.yaml,readonly \
     --volume /data:/data \
     whoi/phyto-arm:latest \
-    /bin/bash -c "cd /launchpad && python3 server.py --package phyto_arm --config /app/mounted_config.yaml /app/configs/example.yaml --start roscore,main,rosbag"
-
+    $COMMAND
